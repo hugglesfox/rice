8fcc1530076b55e5ed718eec777ce680dcf0ccfa
Hayley Hughes <hayley@foxes.systems>
2021-02-20 16:27:23 +1100

    Add tests

---
+++ tests/test_template.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+from rice import template
+
+
+class TestTemplateMethod(unittest.TestCase):
+    def setUp(self):
+        os.environ["RICE_TEST"] = "Test"
+
+        with open("test.html", "w") as fp:
+            fp.write("<h1>Hello {{ RICE_TEST }}</h1>")
+        with open("test.yaml", "w") as fp:
+            fp.write("user: '{{ RICE_TEST }}'")
+
+    def test_template(self):
+        template(["test.html", "test.yaml"])
+
+        with open("test.html") as fp:
+            self.assertEqual(fp.read(), "<h1>Hello Test</h1>")
+        with open("test.yaml") as fp:
+            self.assertEqual(fp.read(), "user: 'Test'")
+
+    def tearDown(self):
+        del os.environ["RICE_TEST"]
+        os.remove("test.html")
+        os.remove("test.yaml")
