c538c6d45a0a42f7d51b74f915a703f67ba2c19f
Hayley Hughes <hayley@foxes.systems>
2022-03-07 13:37:00 +1100

    Add dotenv file loading

--- rice/__main__.py
+++ rice/__main__.py
@@ -4,16 +4,23 @@ Rice aids automating configuration files by combinding environment variables
 with jinja2.
 
 Usage:
-    rice [-hvo] FILE ...
+    rice [-hv] [-f VAR] FILE ...
 
 Options:
   -h --help     Show this screen.
   -v --version  Show version.
+  -f --file     Load envrionment variables from file
 """
 
+import dotenv
 from docopt import docopt
 from .utils import template
 from . import __version__
 
 arguments = docopt(__doc__, version=__version__)
+dotenv.load_dotenv()
+
+if arguments["--file"]:
+    dotenv.dotenv_values(arguments["VAR"])
+
 template(arguments["FILE"])
